753c0125741137d6b152131fcd3e67ff57a89459
Matias Corallo <matiascorallodev@gmail.com>
2023-07-28 22:49:06 -0300

    change auth0 add

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,28 +1,47 @@
-import React from 'react'
-import '../static/css/layout.css'
-import axios from 'axios';
+import { useAuth0 } from "@auth0/auth0-react";
+import "../static/css/layout.css";
+import axios from "axios";
 
 export const Login = () => {
+  const apiUrl = "http://localhost:3000/test";
+  const { user, isAuthenticated, isLoading } = useAuth0();
+  const { loginWithRedirect } = useAuth0();
+  const { logout } = useAuth0();
 
-    const apiUrl = 'http://localhost:3000/test';
+  const data = { test: "test" };
 
-    const data = { "test": "test" };
-
-    const fetchDataFromApi = async () => {
-
-      axios.put(apiUrl, data)
-      .then(response => {
+  const fetchDataFromApi = async () => {
+    axios
+      .put(apiUrl, data)
+      .then((response) => {
         console.log(response.data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
-    }
+  };
 
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
   return (
-    <div className='test'>
-        <a href='http://localhost:3000/auth/microsoft'>logeate</a>
-        <button onClick={fetchDataFromApi}>test</button>
+    <div className="test">
+      {isAuthenticated && (
+        <div>
+          <img src={user.picture} alt={user.name} />
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+        </div>
+      )}
+      <button onClick={() => loginWithRedirect()}>logeate</button>
+      <button
+        onClick={() =>
+          logout({ logoutParams: { returnTo: window.location.origin } })
+        }
+      >
+        Log Out
+      </button>
+      <button onClick={fetchDataFromApi}>test</button>
     </div>
-  )
-}
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Auth0Provider
+        domain="dev-efoclx3oif6amatk.us.auth0.com"
+        clientId="LuMaEkoKnJ0OPfVRoUfNclpRA0BzhpRm"
+        authorizationParams={{
+          redirect_uri: window.location.origin
+        }}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);

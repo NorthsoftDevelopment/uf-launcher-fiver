a6427847a30910f9631a0da429db7e87d40a35af
Korita <korita@inhonia.online>
2023-08-06 11:50:14 -0600

    new launch options

--- public/launchers/2/main.js
+++ public/launchers/2/main.js
@@ -1,3 +1,41 @@
+var valorVersion = document.getElementById('seleccionVersion').value;
+var valorRam = document.getElementById('seleccionRam').value;
+var valorRoot = document.getElementById('seleccionRoute').value;
+var username = document.getElementById('user').value;
+var valorforge = document.getElementById('seleccionForge').value;
+
+var forgeversion = ''
+var forgeFile = ""
+var pathJava = ""
+
+switch (valorforge) {
+    case '1.19.2':
+        forgeversion = "https://www.dropbox.com/s/9ztxy844vch1v1a/forge-1.19.2.zip?dl=1";
+        forgeFile = "\\forge-1.19.2.jar";
+        pathJava = "C:/InhoniaLauncher/Java/bin/javaw.exe";
+        break;
+    case '1.18.2':
+        forgeversion = "https://www.dropbox.com/s/1mrcbex3addqw6h/forge-1.18.2.zip?dl=1";
+        forgeFile = "\\forge-1.18.2.jar";
+        pathJava = "C:/InhoniaLauncher/Java/bin/javaw.exe";
+        break;
+    case '1.16.5':
+        forgeversion = "https://www.dropbox.com/s/a76mfml9u7gq9tx/forge-1.16.5.zip?dl=1";
+        forgeFile = "\\forge-1.16.5.jar";
+        pathJava = "C:/InhoniaLauncher/Java/bin/javaw.exe";
+        break;
+    case '1.12.2':
+        forgeversion = "https://www.dropbox.com/s/awazsla4g5he2hd/forge-1.12.2.zip?dl=1";
+        forgeFile = "\\forge-1.12.2.jar";
+        pathJava = "C:/Program Files/Java/jre-1.8/bin/javaw.exe";
+        break;
+
+    default:
+        forgeversion = "https://www.dropbox.com/s/9ztxy844vch1v1a/forge-1.19.2.zip?dl=1";
+        forgeFile = "\\forge-1.19.2.jar";
+        break;
+}
+
 function premiun() {
 
     const fs = require('fs');
@@ -5,43 +43,6 @@ function premiun() {
 
     const folderPath = 'C:/InhoniaLauncher/Instance/MinecraftVanilla';
     fs.mkdirSync(path.dirname(folderPath), { recursive: true });
-
-    var valorVersion = document.getElementById('seleccionVersion').value;
-    var valorRam = document.getElementById('seleccionRam').value;
-    var valorRoot = document.getElementById('seleccionRoute').value;
-    var valorforge = document.getElementById('seleccionForge').value;
-
-    var forgeversion = ''
-    var forgeFile = ""
-    var pathJava = ""
-
-    switch (valorforge) {
-        case '1.19.2':
-          forgeversion = "https://www.dropbox.com/s/9ztxy844vch1v1a/forge-1.19.2.zip?dl=1";
-          forgeFile = "\\forge-1.19.2.jar";
-          pathJava = "C:/InhoniaLauncher/Java/bin/javaw.exe";
-          break;
-        case '1.18.2':
-          forgeversion = "https://www.dropbox.com/s/1mrcbex3addqw6h/forge-1.18.2.zip?dl=1";
-          forgeFile = "\\forge-1.18.2.jar";
-          pathJava = "C:/InhoniaLauncher/Java/bin/javaw.exe";
-          break;
-        case '1.16.5':
-          forgeversion = "https://www.dropbox.com/s/a76mfml9u7gq9tx/forge-1.16.5.zip?dl=1";
-          forgeFile = "\\forge-1.16.5.jar";
-          pathJava = "C:/InhoniaLauncher/Java/bin/javaw.exe";
-          break;
-        case '1.12.2':
-          forgeversion = "https://www.dropbox.com/s/awazsla4g5he2hd/forge-1.12.2.zip?dl=1";
-          forgeFile = "\\forge-1.12.2.jar";
-          pathJava = "C:/Program Files/Java/jre-1.8/bin/javaw.exe";
-          break;
-
-        default:
-          forgeversion = "https://www.dropbox.com/s/9ztxy844vch1v1a/forge-1.19.2.zip?dl=1";
-          forgeFile = "\\forge-1.19.2.jar";
-          break;
-      }
 
     const { Client, Authenticator } = require('minecraft-launcher-core');
     const launcher = new Client();
@@ -122,100 +123,62 @@ function nopremiun() {
     const folderPath = 'C:/InhoniaLauncher/Instance/MinecraftVanilla';
     fs.mkdirSync(path.dirname(folderPath), { recursive: true });
 
-    var valorVersion = document.getElementById('seleccionVersion').value;
-    var valorRam = document.getElementById('seleccionRam').value;
-    var valorRoot = document.getElementById('seleccionRoute').value;
-    var username = document.getElementById('user').value;
-    var valorforge = document.getElementById('seleccionForge').value;
-
-    var forgeversion = ''
-    var forgeFile = ""
-    var pathJava = ""
-
-    switch (valorforge) {
-        case '1.19.2':
-          forgeversion = "https://www.dropbox.com/s/9ztxy844vch1v1a/forge-1.19.2.zip?dl=1";
-          forgeFile = "\\forge-1.19.2.jar";
-          pathJava = "C:/InhoniaLauncher/Java/bin/javaw.exe";
-          break;
-        case '1.18.2':
-          forgeversion = "https://www.dropbox.com/s/1mrcbex3addqw6h/forge-1.18.2.zip?dl=1";
-          forgeFile = "\\forge-1.18.2.jar";
-          pathJava = "C:/InhoniaLauncher/Java/bin/javaw.exe";
-          break;
-        case '1.16.5':
-          forgeversion = "https://www.dropbox.com/s/a76mfml9u7gq9tx/forge-1.16.5.zip?dl=1";
-          forgeFile = "\\forge-1.16.5.jar";
-          pathJava = "C:/InhoniaLauncher/Java/bin/javaw.exe";
-          break;
-        case '1.12.2':
-          forgeversion = "https://www.dropbox.com/s/awazsla4g5he2hd/forge-1.12.2.zip?dl=1";
-          forgeFile = "\\forge-1.12.2.jar";
-          pathJava = "C:/Program Files/Java/jre-1.8/bin/javaw.exe";
-          break;
-
-        default:
-          forgeversion = "https://www.dropbox.com/s/9ztxy844vch1v1a/forge-1.19.2.zip?dl=1";
-          forgeFile = "\\forge-1.19.2.jar";
-          break;
-      }
-
     const { Client, Authenticator } = require('minecraft-launcher-core');
     const launcher = new Client();
 
-        let opts = {
-            authorization: Authenticator.getAuth(username),
-            overrides: {
-                detached: false,
-            },
-            root: valorRoot,
-            clientPackage: forgeversion,
-            version: {
-                number: valorVersion,
-                type: "release"
-            },
-            forge: valorRoot + forgeFile,
-            removePackage: true,
-            javaPath: pathJava,
-            memory: {
-                max: valorRam,
-                min: "3G"
-            },
+    let opts = {
+        authorization: Authenticator.getAuth(username),
+        overrides: {
+            detached: false,
+        },
+        root: valorRoot,
+        clientPackage: forgeversion,
+        version: {
+            number: valorVersion,
+            type: "release"
+        },
+        forge: valorRoot + forgeFile,
+        removePackage: true,
+        javaPath: pathJava,
+        memory: {
+            max: valorRam,
+            min: "3G"
+        },
 
-        }
+    }
 
-        launcher.launch(opts);
+    launcher.launch(opts);
 
-        launcher.on('debug', (e) => console.log(e));
-        launcher.on('data', (e) => console.log(e));
-        launcher.on('progress', (e) => {
+    launcher.on('debug', (e) => console.log(e));
+    launcher.on('data', (e) => console.log(e));
+    launcher.on('progress', (e) => {
 
-            var progress = e;
+        var progress = e;
 
-            var progressBar = document.getElementById('progress-bar');
-            var progressText = document.getElementById('progress-text');
+        var progressBar = document.getElementById('progress-bar');
+        var progressText = document.getElementById('progress-text');
 
-            var porcentaje = Math.floor((progress.task / progress.total) * 100);
+        var porcentaje = Math.floor((progress.task / progress.total) * 100);
 
-            progressBar.style.width = porcentaje + '%';
-            progressText.innerText = porcentaje + '%';
+        progressBar.style.width = porcentaje + '%';
+        progressText.innerText = porcentaje + '%';
 
-        })
-        launcher.on('data', (e) => {
-            document.getElementById("status").textContent = e
-            document.getElementById("status-content").style.display = "flex"
-            document.getElementById("download-screen").style.display = "none";
-        })
+    })
+    launcher.on('data', (e) => {
+        document.getElementById("status").textContent = e
+        document.getElementById("status-content").style.display = "flex"
+        document.getElementById("download-screen").style.display = "none";
+    })
 
-        launcher.on('debug', (e) => {
-            document.getElementById("download-screen").style.display = "flex";
-            document.getElementById("descarga").textContent = e
+    launcher.on('debug', (e) => {
+        document.getElementById("download-screen").style.display = "flex";
+        document.getElementById("descarga").textContent = e
 
-        })
-        launcher.on('close', (e) => {
-            document.getElementById("status").textContent = null
-            document.getElementById("status-content").style.display = "none"
-        })
+    })
+    launcher.on('close', (e) => {
+        document.getElementById("status").textContent = null
+        document.getElementById("status-content").style.display = "none"
+    })
 
 }
 
--- public/launchers/3/main.js
+++ public/launchers/3/main.js
@@ -1,86 +1,190 @@
-const Cookies = require('js-cookie')
-const axios = require('axios')
 
-const email = Cookies.get('email')
+//Variables de usuario de lanzamiento
+var valorVersion = document.getElementById('seleccionVersion').value;
+var valorRam = document.getElementById('seleccionRam').value;
+var valorRoot = document.getElementById('seleccionRoute').value;
+var username = document.getElementById('user').value;
+var valorforge = document.getElementById('seleccionForge').value;
 
-console.log(email)
+var forgeversion = ''
+var forgeFile = ""
+var pathJava = ""
 
-const api = 'https://inhonia-launcher-api.vercel.app/instance/whitelist'
-const docUbi = 'oEFiPXiavEfQlfHQ0mgC'
+switch (valorforge) {
+    case '1.19.2':
+        forgeversion = "https://www.dropbox.com/s/9ztxy844vch1v1a/forge-1.19.2.zip?dl=1";
+        forgeFile = "\\forge-1.19.2.jar";
+        pathJava = "C:/InhoniaLauncher/Java/bin/javaw.exe";
+        break;
+    case '1.18.2':
+        forgeversion = "https://www.dropbox.com/s/1mrcbex3addqw6h/forge-1.18.2.zip?dl=1";
+        forgeFile = "\\forge-1.18.2.jar";
+        pathJava = "C:/InhoniaLauncher/Java/bin/javaw.exe";
+        break;
+    case '1.16.5':
+        forgeversion = "https://www.dropbox.com/s/a76mfml9u7gq9tx/forge-1.16.5.zip?dl=1";
+        forgeFile = "\\forge-1.16.5.jar";
+        pathJava = "C:/InhoniaLauncher/Java/bin/javaw.exe";
+        break;
+    case '1.12.2':
+        forgeversion = "https://www.dropbox.com/s/awazsla4g5he2hd/forge-1.12.2.zip?dl=1";
+        forgeFile = "\\forge-1.12.2.jar";
+        pathJava = "C:/Program Files/Java/jre-1.8/bin/javaw.exe";
+        break;
 
-const docRef = [docUbi]
-
-axios.post(api, docRef)
-
-      .then((response) => {
-
-        console.log(response.data);
-
-        const whitelist = response.data
-
-        if (email === 'koraook@gmail.com') {
-
-            document.getElementById('tabla').style.display = "flex"
-            console.log('Usuario Administrador')
-        
-        } else {
-        
-            console.log('not admin user')
-        
-        }
-        
-        
-        if (whitelist.includes(email)) {
-
-            console.log('Usuario en whitelist');
-
-          } else {
-
-            window.location.href = '/';
-          }
-
-        console.log('Sucessfully Get')
-      })
-
-      .catch((error) => {
-        console.error(error);
-
-        window.location.href = '/'
-
-
-      });
-
-
-
-
-function useradd() {
-
-    var usermail = document.getElementById('adduser').value
-
-    var email = [usermail]
-
-    const datasend = {
-        email: email,
-        ubicacion: 'oEFiPXiavEfQlfHQ0mgC'
-    }
-
-    const api = 'https://inhonia-launcher-api.vercel.app/instance/adduser'
-
-    axios
-      .post(api, datasend)
-
-      .then((response) => {
-        console.log(response.data);
-
-        console.log('Send Sucessfully')
-      })
-
-      .catch((error) => {
-        console.error(error);
-      });
-
+    default:
+        forgeversion = "https://www.dropbox.com/s/9ztxy844vch1v1a/forge-1.19.2.zip?dl=1";
+        forgeFile = "\\forge-1.19.2.jar";
+        break;
 }
 
 
+//Funciones de lanzamientos
+
+function premiun() {
+
+    const fs = require('fs');
+    const path = require('path');
+
+    const folderPath = 'C:/InhoniaLauncher/Instance/MinecraftVanilla';
+    fs.mkdirSync(path.dirname(folderPath), { recursive: true });
+
+    const { Client, Authenticator } = require('minecraft-launcher-core');
+    const launcher = new Client();
+
+    const { Auth } = require("msmc");
+
+    const authManager = new Auth("select_account");
+
+    authManager.launch("raw").then(async xboxManager => {
+
+        const token = await xboxManager.getMinecraft();
+
+        let opts = {
+
+            authorization: token.mclc(),
+            overrides: {
+                detached: false,
+            },
+            root: valorRoot,
+            clientPackage: forgeversion,
+            version: {
+                number: valorVersion,
+                type: "release"
+            },
+            forge: valorRoot + forgeFile,
+            removePackage: true,
+            javaPath: pathJava,
+            memory: {
+                max: valorRam,
+                min: "3G"
+            },
+
+        }
+
+        launcher.launch(opts);
+
+        launcher.on('debug', (e) => console.log(e));
+        launcher.on('data', (e) => console.log(e));
+        launcher.on('progress', (e) => {
+
+            var progress = e;
+
+            var progressBar = document.getElementById('progress-bar');
+            var progressText = document.getElementById('progress-text');
+
+            var porcentaje = Math.floor((progress.task / progress.total) * 100);
+
+            progressBar.style.width = porcentaje + '%';
+            progressText.innerText = porcentaje + '%';
+
+        })
+        launcher.on('data', (e) => {
+            document.getElementById("status").textContent = e
+            document.getElementById("status-content").style.display = "flex"
+            document.getElementById("download-screen").style.display = "none";
+        })
+
+        launcher.on('debug', (e) => {
+            document.getElementById("download-screen").style.display = "flex";
+            document.getElementById("descarga").textContent = e
+
+        })
+        launcher.on('close', (e) => {
+            document.getElementById("status").textContent = null
+            document.getElementById("status-content").style.display = "none"
+        })
+
+
+
+    })
+}
+
+function nopremiun() {
+
+    const fs = require('fs');
+    const path = require('path');
+
+    const folderPath = 'C:/InhoniaLauncher/Instance/MinecraftVanilla';
+    fs.mkdirSync(path.dirname(folderPath), { recursive: true });
+
+    const { Client, Authenticator } = require('minecraft-launcher-core');
+    const launcher = new Client();
+
+    let opts = {
+        authorization: Authenticator.getAuth(username),
+        overrides: {
+            detached: false,
+        },
+        root: valorRoot,
+        clientPackage: forgeversion,
+        version: {
+            number: valorVersion,
+            type: "release"
+        },
+        forge: valorRoot + forgeFile,
+        removePackage: true,
+        javaPath: pathJava,
+        memory: {
+            max: valorRam,
+            min: "3G"
+        },
+
+    }
+
+    launcher.launch(opts);
+
+    launcher.on('debug', (e) => console.log(e));
+    launcher.on('data', (e) => console.log(e));
+    launcher.on('progress', (e) => {
+
+        var progress = e;
+
+        var progressBar = document.getElementById('progress-bar');
+        var progressText = document.getElementById('progress-text');
+
+        var porcentaje = Math.floor((progress.task / progress.total) * 100);
+
+        progressBar.style.width = porcentaje + '%';
+        progressText.innerText = porcentaje + '%';
+
+    })
+    launcher.on('data', (e) => {
+        document.getElementById("status").textContent = e
+        document.getElementById("status-content").style.display = "flex"
+        document.getElementById("download-screen").style.display = "none";
+    })
+
+    launcher.on('debug', (e) => {
+        document.getElementById("download-screen").style.display = "flex";
+        document.getElementById("descarga").textContent = e
+
+    })
+    launcher.on('close', (e) => {
+        document.getElementById("status").textContent = null
+        document.getElementById("status-content").style.display = "none"
+    })
+
+}
 
 

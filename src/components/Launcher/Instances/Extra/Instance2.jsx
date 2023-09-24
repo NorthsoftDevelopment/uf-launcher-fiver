6fea0e2cee2626e8c4af16bd0114031e203d2e57
koriita0 <korita@inhonia.online>
2023-09-23 19:52:55 -0600

    xd

--- src/components/Launcher/Instances/Extra/Instance2.jsx
+++ src/components/Launcher/Instances/Extra/Instance2.jsx
@@ -36,10 +36,10 @@ export const Instance2 = () => {
 
                 <LauncherDesigned
 
-                    title='Lalaland (Gran Evento!)'
-                    background='https://cdn.discordapp.com/attachments/1150101451033559260/1153794895417454592/el_laberinto_banner.png'
+                    title='Lalaland'
+                    background='https://cdn.discordapp.com/attachments/1107684864506998824/1144483633604857937/mc_launcher_photo_1_.png'
                     autor='Eladina • Comunidad • Servidor'
-                    sponsorDesc='El Laberinto'
+                    sponsorDesc='Lalaland Servidor'
                     sponsorIMG='https://cdn.discordapp.com/attachments/1035201776955183134/1150566470863028284/image.png'
                     sponsorTitle='Lalaland Server'
                     valorRoot='C:/InhoniaLauncher/Instance/2'

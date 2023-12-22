b7005cf710c47ca0e16d1eaa88506d5fef1a0cea
ダイレクトコネクト <52444606+TokyoTF@users.noreply.github.com>
2023-12-22 18:12:31 -0300

    update

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -1,12 +1,104 @@
-import Cookies from 'js-cookie';
-import React from 'react'
-import Swal from 'sweetalert2';
+import Cookies from "js-cookie";
+import React from "react";
+import Swal from "sweetalert2";
 
 export const Home = () => {
   return (
-    <div>
-      <h1>Bienvenido a tu launcher</h1>
-      <button>lANZAR JUEGO</button>
+    <div className="welcome">
+      <div className="bg-img">
+        <img src="https://i.imgur.com/R5YlI0J.jpg" alt="" />
+      </div>
+      <div className="welcome-content content">
+        <h2>Bienvenido a tu launcher</h2>
+        <div className="list-instances">
+          <swiper-container
+            centeredSlides="true"
+            speed="500"
+            loop="true"
+            slides-per-view="3"
+            space-between="5"
+            width="1540"
+          >
+            <swiper-slide>
+              <div className="instance">
+                <button className="play">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M2.7 10.3a2.41 2.41 0 0 0 0 3.41l7.59 7.59a2.41 2.41 0 0 0 3.41 0l7.59-7.59a2.41 2.41 0 0 0 0-3.41l-7.59-7.59a2.41 2.41 0 0 0-3.41 0Z" />
+                  </svg>
+                  <span>Jugar</span>
+                </button>
+              </div>
+            </swiper-slide>
+            <swiper-slide>
+            <div className="instance">
+                <button className="play">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M2.7 10.3a2.41 2.41 0 0 0 0 3.41l7.59 7.59a2.41 2.41 0 0 0 3.41 0l7.59-7.59a2.41 2.41 0 0 0 0-3.41l-7.59-7.59a2.41 2.41 0 0 0-3.41 0Z" />
+                  </svg>
+                  <span>Jugar</span>
+                </button>
+              </div>
+            </swiper-slide>
+            <swiper-slide>
+            <div className="instance">
+                <button className="play">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M2.7 10.3a2.41 2.41 0 0 0 0 3.41l7.59 7.59a2.41 2.41 0 0 0 3.41 0l7.59-7.59a2.41 2.41 0 0 0 0-3.41l-7.59-7.59a2.41 2.41 0 0 0-3.41 0Z" />
+                  </svg>
+                  <span>Jugar</span>
+                </button>
+              </div>
+            </swiper-slide>
+            <swiper-slide>
+            <div className="instance">
+                <button className="play">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M2.7 10.3a2.41 2.41 0 0 0 0 3.41l7.59 7.59a2.41 2.41 0 0 0 3.41 0l7.59-7.59a2.41 2.41 0 0 0 0-3.41l-7.59-7.59a2.41 2.41 0 0 0-3.41 0Z" />
+                  </svg>
+                  <span>Jugar</span>
+                </button>
+              </div>
+            </swiper-slide>
+            <swiper-slide>
+            <div className="instance">
+                <button className="play">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M2.7 10.3a2.41 2.41 0 0 0 0 3.41l7.59 7.59a2.41 2.41 0 0 0 3.41 0l7.59-7.59a2.41 2.41 0 0 0 0-3.41l-7.59-7.59a2.41 2.41 0 0 0-3.41 0Z" />
+                  </svg>
+                  <span>Jugar</span>
+                </button>
+              </div>
+            </swiper-slide>
+          </swiper-container>
+        </div>
+      </div>
+
+      <button>Lanzar juego</button>
     </div>
-  )
-}
+  );
+};

efcb5f5901763bc25e5d9a4692ddaa75afd4666b
Korita <126135587+Koriita0@users.noreply.github.com>
2023-12-23 15:49:27 -0700

    Update home.jsx

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -32,7 +32,7 @@ export const Home = () => {
 
         if (setting.launcherType === 'sk') {
 
-          const child = execFile(result.folderPath + '/sk.exe', ['--workDir', 'C:/UFLauncher/instance/test'], (error, stdout, stderr) => {
+          const child = execFile(result.folderPath + '\\sk.exe', ['--workDir', 'C:/UFLauncher/instance/test'], (error, stdout, stderr) => {
             const notificationContent = (
               <div>
                 <h4>Juego lanzado correctamente</h4>
@@ -70,7 +70,7 @@ export const Home = () => {
           });
         } else {
 
-          const child = execFile(result.folderPath + '/mc.exe', ['--workDir', 'C:/UFLauncher/instance/test'], (error, stdout, stderr) => {
+          const child = execFile(result.folderPath + '\\mc.exe', ['--workDir', 'C:/UFLauncher/instance/test'], (error, stdout, stderr) => {
             setRunning(false)
             const notificationContent = (
               <div>

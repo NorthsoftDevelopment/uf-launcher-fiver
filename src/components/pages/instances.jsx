a09770fa392aad38e56f9266507001cdcba2f157
koriita0 <korita@inhonia.online>
2023-12-30 10:40:44 -0700

    final changes

--- src/components/pages/instances.jsx
+++ src/components/pages/instances.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Cookies from "js-cookie";
+import toast from "react-hot-toast";
 
 export const InstancesPage = () => {
   const [instances, setInstances] = useState([]);
@@ -33,19 +34,41 @@ export const InstancesPage = () => {
     }
   };
 
+  const seleccionar = (id) => {
+    console.log(id)
+    Cookies.set('instance', id, { expires: 365, sameSite: 'strict' });
+
+    // Llama a react-hot-toast con la notificación
+    toast.success('Instancia Seleccionada', {
+      duration: 4000, // Duración en milisegundos (opcional)
+    });
+  }
+
+  
+
   return (
-    <div className="welcome scroll">
-      <div className="bg-img">
-        <img src={background} alt="" />
-      </div>
-      <div className="instances-container">
-        {instances.map((instance, index) => (
+    <div className="welcome scroll content">
+      <div className="container-idk">
+
+
+
+        <div className="bg-img">
+          <img src={background} alt="" />
+        </div>
+
+
+        <div className="instances-container">
+          {instances.map((instance, index) => (
             <div key={index} className="instance-item">
               <h3>{instance.title}</h3>
               <p>{instance.desc}</p>
+              <button onClick={() => seleccionar(instance.id)}>Seleccionar</button>
               <img src={instance.img}></img>
+
             </div>
-        ))}
+          ))}
+        </div>
+
       </div>
     </div>
   );

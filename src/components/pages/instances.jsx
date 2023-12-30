889b057137ce0580b2a35acdb5359d83e7451bdc
ダイレクトコネクト <52444606+TokyoTF@users.noreply.github.com>
2023-12-30 00:31:17 -0300

    u

--- src/components/pages/instances.jsx
+++ src/components/pages/instances.jsx
@@ -44,6 +44,10 @@ export const InstancesPage = () => {
               <h3>{instance.title}</h3>
               <p>{instance.desc}</p>
               <img src={instance.img}></img>
+              <div className="instance-play">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><polygon points="5 3 19 12 5 21 5 3"/></svg>
+                <p>Jugar</p>
+              </div>
             </div>
         ))}
       </div>

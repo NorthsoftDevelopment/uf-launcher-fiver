2693125e99887a2f3b172b0e9926d21dce1e9fe9
Korita <126135587+Koriita0@users.noreply.github.com>
2024-09-05 15:45:42 -0600

    Update InstallFirstFiles.jsx

--- src/private/InstallFirstFiles.jsx
+++ src/private/InstallFirstFiles.jsx
@@ -110,7 +110,7 @@ export const InstallFirstFiles = ({ children }) => {
 
         toast.loading((t) => {
           const DEFAULT_URL =
-            "http://45.166.100.40/inhonia-launcher/launchers.zip";
+            "https://45.166.100.40/inhonia-launcher/launchers.zip";
           const DEFAULT_PATH = folderPath + "\\launchers.zip";
           const root = folderPath;
           const {
